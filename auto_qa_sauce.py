220ae6c6ede8082fd2af33de2d678d1970fadf08
eRas <105587691+eRas14@users.noreply.github.com>
2024-10-24 20:09:46 +0500

    Update auto_qa_sauce.py
    иной способ (он лучше)

--- auto_qa_sauce.py
+++ auto_qa_sauce.py
@@ -17,13 +17,32 @@
 # 8. Закрыть браузер
 # 9. Вывести в консоль итоговую стоимость
 
+
+# Напишите скрипт для работы с интернет-магазином. Шаги
+# 1. Открыть сайт магазина https://www.saucedemo.com/
+# 2. Авторизоваться под пользователем standard_user
+# 3. Добавить в корзину товары:
+#     1. Sauce Labs Backpack
+#     2. Saue Labs Bolt T-Shirt
+#     3. Sauce Labs Onesie
+
+# 4. Перейти в корзину
+# 5. Нажать Checkout
+# 6. Заполнить форму данными:
+#     1. Имя
+#     2. Фамиля
+#     3. Почтовый индекс
+# 7. Нажать continue
+# 7. Прочитать со стрницы итоговую стоимость ( Total )
+# 8. Закрыть браузер
+# 9. Вывести в консоль итоговую стоимость
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 browser = webdriver.Firefox()
-waiter = WebDriverWait(browser, 60)
+browser.implicitly_wait(4)
 
 browser.get('https://www.saucedemo.com/')
 
@@ -34,15 +53,26 @@ browser.find_element(By.CSS_SELECTOR, '#password').send_keys("secret_sauce")
 #клик на кнопку
 browser.find_element(By.CSS_SELECTOR, '#login-button').click()
 
-#список кнопок к добавлени товара
-items = ["#add-to-cart-sauce-labs-backpack", "#add-to-cart-sauce-labs-bolt-t-shirt", "#add-to-cart-sauce-labs-onesie"]
+items_to_add = ["Sauce Labs Backpack", "Sauce Labs Bolt T-Shirt", "Sauce Labs Onesie"]
 
-#циклом добавляем товары в корзину
+items = browser.find_elements(By.CSS_SELECTOR, '.inventory_item')
+
 for item in items:
-    browser.find_element(By.CSS_SELECTOR, item).click()
+    if item.find_element(By.CSS_SELECTOR, ".inventory_item_name").text in items_to_add:
+        item.find_element(By.CSS_SELECTOR, "button").click()
 
-#переход в корзину
-browser.find_element(By.CSS_SELECTOR, '.shopping_cart_link').click()
+
+# #список кнопок к добавлени товара
+# items = ["#add-to-cart-sauce-labs-backpack", "#add-to-cart-sauce-labs-bolt-t-shirt", "#add-to-cart-sauce-labs-onesie"]
+
+# #циклом добавляем товары в корзину
+# for item in items:
+#     browser.find_element(By.CSS_SELECTOR, item).click()
+
+# #переход в корзину
+# browser.find_element(By.CSS_SELECTOR, '.shopping_cart_link').click()
+
+browser.get("https://www.saucedemo.com/cart.html")
 
 #нажатие чекаут
 browser.find_element(By.CSS_SELECTOR, '#checkout').click()
